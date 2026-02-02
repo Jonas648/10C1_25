18151f4205a3797a8fc022dbfea76040a34517a3
KOEZIGSKOBR4 <insertemail@here.com>
2026-02-02 14:52:18 +0100

    Kommentierung von ButtonObject.java und MinigameScene.java

--- src/main/java/czg/scenes/minigame_scenes/MinigameScene.java
+++ src/main/java/czg/scenes/minigame_scenes/MinigameScene.java
@@ -7,15 +7,21 @@ package czg.scenes.minigame_scenes;
 import czg.scenes.*;
 
 /**
- *
- * @author guest-kaafgt
+ * Ein Minigame Template bestehend aus einer {@code LevelSelectorScene}, welche die einzelnen Level verknüpft
+ * und jeweils drei Leveln.
  */
 public class MinigameScene extends BaseScene {
     LevelSelectorScene levelSelector;
     BaseScene level1;
     BaseScene level2;
     BaseScene level3;
-    
+
+    /**
+     * Einen neues Minigame erstellen.
+     * @param level1 Level 1
+     * @param level2 Level 2
+     * @param level3 Level 3
+     */
     public MinigameScene(BaseScene level1, BaseScene level2, BaseScene level3) {
         this.level1 = level1;
         this.level2 = level2;
@@ -23,7 +29,10 @@ public class MinigameScene extends BaseScene {
 
         this.levelSelector = new LevelSelectorScene(level1, level2, level3);
     }
-    
+
+    /**
+     * Startet das Minigame
+     */
     public void startMinigame() {
         SceneStack.INSTANCE.push(levelSelector);
     }
